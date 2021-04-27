41bf53a4affa5b05234b0f825cdf085ebe7b2a9d
phuong23401 <hoaphuong23401@gmail.com>
2021-04-27 09:15:13 +0700

    add

--- src/IOFile.java
+++ src/IOFile.java
@@ -9,7 +9,8 @@ public class IOFile {
         String url = "C:\\Users\\ADMIN\\OneDrive\\Documents\\BookList.docx";
         try {
             FileInputStream fileInputStream = new FileInputStream(url);
-            BufferedReader reader = new BufferedReader(new InputStreamReader(fileInputStream));
+            InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream, "UTF-8");
+            BufferedReader reader = new BufferedReader(inputStreamReader);
             String line;
             while ((line = reader.readLine()) != null) {
                 ARRAY_LIST.add(line);
@@ -23,10 +24,14 @@ public class IOFile {
 
     public static void writeFile() {
         try {
-            BufferedWriter writer = new BufferedWriter(new FileWriter("Books.doc"));
+            FileOutputStream fileOutputStream = new FileOutputStream("Books.doc");
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(fileOutputStream, "UTF-8");
+            BufferedWriter writer = new BufferedWriter(outputStreamWriter);
             for (String line: ARRAY_LIST) {
                 writer.write(line);
             }
+            fileOutputStream.close();
+            outputStreamWriter.close();
             writer.close();
         } catch (IOException e) {
             e.printStackTrace();
@@ -36,7 +41,9 @@ public class IOFile {
 
     public static void readNewFile () {
         try {
-            BufferedReader reader = new BufferedReader(new FileReader("Books.doc"));
+            FileInputStream fileInputStream = new FileInputStream("Books.doc");
+            InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream, "UTF-8");
+            BufferedReader reader = new BufferedReader(inputStreamReader);
             String line;
             while ((line = reader.readLine()) != null) {
                 System.out.println(line);
